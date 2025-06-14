de74bd30d8a14be7821a4665b0486d80f1e800b6
Yohan Prakoso <prakoso.yohan@gmail.com>
2025-06-14 10:41:29 +0800

    add sample from paper

--- c_sample/sample.c
+++ c_sample/sample.c
@@ -67,6 +67,21 @@ void odometry(float MI[2][4], float T[4], float angle, float output[2]) {
     }
 }
 
+// source: https://www.researchgate.net/publication/348272315_Implementation_Kinematics_Modeling_and_Odometry_of_Four_Omni_Wheel_Mobile_Robot_on_The_Trajectory_Planning_and_Motion_Control_Based_Microcontroller
+void odometry2(float T[4], float angle_robot, float output[2]) {
+    float gain = 0.6; // ini g ada di papernya, cuma setelah dicoba kayaknya butuh
+    float x = 0;
+    float y = 0;
+    for(int i = 0; i < 4; i++){
+        float a_rad = (45 + angle_robot + i * 90)  * M_PI / 180; // 45 karena sudut roda pertama terhadap roda 45, 90 karena jarak masing2 roda 90 
+        x += -sinf(a_rad) * T[i] / 2;
+        y += cos(a_rad) * T[i] / 2;
+    }
+
+    output[0] = x * gain;
+    output[1] = y * gain;
+}
+
 int main() {
     int numOfWheels = 4;
     float motor[4];
@@ -94,6 +109,14 @@ int main() {
     float speedOutput[2];
     odometry(
       m_odometry, 
+      motor,
+      0,
+      speedOutput
+    );
+    printf("------------ speed output ------------\n");
+    printf("x: %.2f, y: %.2f\n", speedOutput[0], speedOutput[1]);
+
+    odometry2(
       motor,
       0,
       speedOutput
